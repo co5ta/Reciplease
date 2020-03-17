206a4a5e8eb432fa867744093fe258eea43df910
co5ta <costamonzili@gmail.com>
2020-03-17 20:10:52 +0100

    Install Alamofire

--- Reciplease/Reciplease/SearchResultViewController.swift
+++ Reciplease/Reciplease/SearchResultViewController.swift
@@ -16,5 +16,17 @@ class SearchResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        getRecipes()
+    }
+    
+    @objc func getRecipes(){
+        let parameters = [
+            "app_id": "2e2b7a80",
+            "app_key": "e0af9c94daa4af7d0dc971bb15389075",
+            "q": "chicken"
+        ]
+        AF.request("https://api.edamam.com/search", parameters: parameters).response { (data) in
+            debugPrint(data)
+        }
     }
 }
