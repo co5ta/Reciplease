e0e3a543f0e9c8df566652ed5079472745fab609
co5ta <costamonzili@gmail.com>
2020-04-11 12:33:11 +0200

    Add support of dynamic type text style on search form

--- Reciplease/Reciplease/Controller/SearchFormViewController.swift
+++ Reciplease/Reciplease/Controller/SearchFormViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 /// Controller of the search form screen
 class SearchFormViewController: UIViewController {
     
+    /// Main scroll view
+    var scrollView = UIScrollView()
+    /// View which contains all the subviews in the scroll view
+    var container = UIView()
     /// Section of the ingredient textField
     var textFieldView = UIView()
     /// Label of the textField
@@ -51,40 +55,46 @@ extension SearchFormViewController {
     
     /// Sets up the views
     private func setUpViews() {
-        setUpNavigationItem()
-        setUpRootView()
+        setUpController()
+        setUpScrollView()
+        setUpContainer()
         setUpTextFieldSection()
         setUpTextFieldLabel()
+        setUpAddButton()
         setUpTextField()
         setUpTextFieldBorder()
-        setUpAddButton()
-        setUpIngredientsSection()
+        setUpIngredientsView()
         setUpIngredientsTitle()
         setUpIngredientsList()
         setUpClearButton()
         setUpSubmitButton()
     }
     
-    /// Sets up the navigation item
-    private func setUpNavigationItem() {
+    /// Sets up default properties of the controller
+    private func setUpController() {
         navigationItem.title = "Reciplease"
-        navigationItem.backBarButtonItem = UIBarButtonItem(
-            title: "",
-            style: .plain,
-            target: nil,
-            action: nil)
+        let button = UIBarButtonItem(title: "", style: .plain,target: nil, action: nil)
+        navigationItem.backBarButtonItem = button
+        view.backgroundColor = .systemBackground
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
     }
     
-    /// Sets up the root view
-    private func setUpRootView() {
-        view.backgroundColor = .systemBackground
-        let tapView = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tapView)
+    /// Sets up the scroll view
+    private func setUpScrollView() {
+        view.addSubview(scrollView)
+        setUpScrollViewConstraints()
+    }
+    
+    ///Sets up the container
+    private func setUpContainer() {
+        scrollView.addSubview(container)
+        setUpContainerConstraints()
     }
     
     /// Sets up the text field section
     private func setUpTextFieldSection() {
-        view.addSubview(textFieldView)
+        container.addSubview(textFieldView)
         textFieldView.backgroundColor = .tertiarySystemBackground
         textFieldView.layer.shadowColor = UIColor.separator.cgColor
         textFieldView.layer.shadowOffset = CGSize(width: 1, height: 1)
@@ -96,41 +106,45 @@ extension SearchFormViewController {
     
     /// Sets up the text field label
     private func setUpTextFieldLabel() {
-        view.addSubview(textFieldLabel)
-        textFieldLabel.text = "What's on your fridge ?"
+        container.addSubview(textFieldLabel)
+        textFieldLabel.text = "What's in your fridge ?"
+        textFieldLabel.numberOfLines = 0
         textFieldLabel.adjustsFontForContentSizeCategory = true
         textFieldLabel.font = UIFont.preferredFont(forTextStyle: .title1)
         setUpTextFieldLabelConstraints()
     }
     
-    /// Sets up the text field
-    private func setUpTextField() {
-        view.addSubview(textField)
-        textField.placeholder = "Lemon, Cheese, Sausages..."
-        setUpTextFieldConstraints()
-    }
-    
-    /// Sets up the text field border
-    private func setUpTextFieldBorder() {
-        view.addSubview(textFieldBorder)
-        textFieldBorder.backgroundColor = .opaqueSeparator
-        setUpTextFieldBorderConstraints()
-    }
-    
     /// Sets up the add button
     private func setUpAddButton() {
-        view.addSubview(addButton)
+        container.addSubview(addButton)
         addButton.setTitle("Add", for: .normal)
         addButton.tintColor = .white
+        addButton.contentEdgeInsets = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+        addButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+        addButton.titleLabel?.adjustsFontForContentSizeCategory = true
         addButton.backgroundColor = Config.globalTintColor
         addButton.layer.cornerRadius = 5
         addButton.addTarget(self, action: #selector(addIngredient), for: .touchUpInside)
         setUpAddButtonConstraints()
     }
     
+    /// Sets up the text field
+    private func setUpTextField() {
+        container.addSubview(textField)
+        textField.placeholder = "Lemon, Cheese, Sausages..."
+        setUpTextFieldConstraints()
+    }
+    
+    /// Sets up the text field border
+    private func setUpTextFieldBorder() {
+        container.addSubview(textFieldBorder)
+        textFieldBorder.backgroundColor = .opaqueSeparator
+        setUpTextFieldBorderConstraints()
+    }
+    
     /// Sets up the ingredients section
-    private func setUpIngredientsSection() {
-        view.addSubview(ingredientsView)
+    private func setUpIngredientsView() {
+        container.addSubview(ingredientsView)
         ingredientsView.backgroundColor = .tertiarySystemBackground
         ingredientsView.layer.shadowColor = UIColor.separator.cgColor
         ingredientsView.layer.shadowOffset = CGSize(width: 1, height: 1)
@@ -142,39 +156,51 @@ extension SearchFormViewController {
     
     /// Sets up the ingredients title
     private func setUpIngredientsTitle() {
-        view.addSubview(ingredientsTitleLabel)
-        ingredientsTitleLabel.text = "Your ingredients :"
+        container.addSubview(ingredientsTitleLabel)
+        ingredientsTitleLabel.text = "Your ingredients"
+        ingredientsTitleLabel.numberOfLines = 0
         ingredientsTitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
-        setUpIngredientsTitleConstraints()
+        ingredientsTitleLabel.adjustsFontForContentSizeCategory = true
+        setUpIngredientsTitleLabelConstraints()
     }
     
     /// Sets up the ingredients list
     private func setUpIngredientsList() {
-        view.addSubview(ingredientsList)
+        container.addSubview(ingredientsList)
         ingredientsList.font = UIFont.preferredFont(forTextStyle: .body)
         ingredientsList.backgroundColor = .tertiarySystemBackground
+        ingredientsList.adjustsFontForContentSizeCategory = true
         ingredientsList.isSelectable = false
+        ingredientsList.isScrollEnabled = false
         setUpIngredientsListConstraints()
     }
     
     /// Sets up the clear button
     private func setUpClearButton() {
-        view.addSubview(clearButton)
+        container.addSubview(clearButton)
         clearButton.tintColor = .white
         clearButton.backgroundColor = Config.cancelButtonColor
-        clearButton.setTitle("Clear", for: .normal)
+        clearButton.setTitle("Cancel", for: .normal)
+        clearButton.titleLabel?.adjustsFontForContentSizeCategory = true
+        clearButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        clearButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
         clearButton.layer.cornerRadius = 5
+        clearButton.contentEdgeInsets = UIEdgeInsets(top: 15, left: 1, bottom: 15, right: 1)
         clearButton.addTarget(self, action: #selector(removeIngredients), for: .touchUpInside)
         setUpClearButtonConstraints()
     }
     
     /// Sets up the submit button
     private func setUpSubmitButton() {
-        view.addSubview(submitButton)
+        container.addSubview(submitButton)
         submitButton.tintColor = .white
         submitButton.backgroundColor = Config.globalTintColor
-        submitButton.setTitle("Search for recipes", for: .normal)
+        submitButton.setTitle("Search", for: .normal)
+        submitButton.titleLabel?.adjustsFontForContentSizeCategory = true
+        submitButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        submitButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
         submitButton.layer.cornerRadius = 5
+        submitButton.contentEdgeInsets = UIEdgeInsets(top: 15, left: 1, bottom: 15, right: 1)
         submitButton.addTarget(self, action: #selector(goToSearchResultScreen), for: .touchUpInside)
         setUpSubmitButtonConstraints()
     }
@@ -183,20 +209,40 @@ extension SearchFormViewController {
 // MARK: - Constraints
 extension SearchFormViewController {
  
+    ///
+    private func setUpScrollViewConstraints() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    /// Sets up the container
+    private func setUpContainerConstraints() {
+        container.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            container.heightAnchor.constraint(
+                greaterThanOrEqualTo: view.safeAreaLayoutGuide.heightAnchor),
+            container.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            container.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            container.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
+        ])
+    }
+    
     /// Sets up constraints for the text field section
     private func setUpTextFieldViewConstraints() {
         textFieldView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            textFieldView.heightAnchor.constraint(
-                equalTo: textFieldView.widthAnchor,
-                multiplier: 2/5),
             textFieldView.topAnchor.constraint(
-                equalToSystemSpacingBelow: view.readableContentGuide.topAnchor,
+                equalToSystemSpacingBelow: container.topAnchor,
                 multiplier: 3),
             textFieldView.leadingAnchor.constraint(
-                equalToSystemSpacingAfter: view.leadingAnchor,
+                equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor,
                 multiplier: 2),
-            view.trailingAnchor.constraint(
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(
                 equalToSystemSpacingAfter: textFieldView.trailingAnchor,
                 multiplier: 2)
         ])
@@ -211,6 +257,26 @@ extension SearchFormViewController {
                 multiplier: 2),
             textFieldLabel.leadingAnchor.constraint(
                 equalToSystemSpacingAfter: textFieldView.leadingAnchor,
+                multiplier: 2),
+            textFieldView.trailingAnchor.constraint(
+                equalToSystemSpacingAfter: textFieldLabel.trailingAnchor,
+                multiplier: 1)
+        ])
+    }
+    
+    /// Sets up constraints for add button
+    private func setUpAddButtonConstraints() {
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            addButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 45),
+            addButton.topAnchor.constraint(
+                equalToSystemSpacingBelow: textFieldLabel.bottomAnchor,
+                multiplier: 2),
+            textFieldView.trailingAnchor.constraint(
+                equalToSystemSpacingAfter: addButton.trailingAnchor,
+                multiplier: 2),
+            textFieldView.bottomAnchor.constraint(
+                equalToSystemSpacingBelow: addButton.bottomAnchor,
                 multiplier: 2)
         ])
     }
@@ -219,9 +285,13 @@ extension SearchFormViewController {
     private func setUpTextFieldConstraints() {
         textField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: addButton.topAnchor),
             textField.leadingAnchor.constraint(
                 equalToSystemSpacingAfter: textFieldView.leadingAnchor,
-                multiplier: 2)
+                multiplier: 2),
+            addButton.leadingAnchor.constraint(
+                equalToSystemSpacingAfter: textField.trailingAnchor,
+                multiplier: 2),
         ])
     }
     
@@ -232,28 +302,10 @@ extension SearchFormViewController {
             textFieldBorder.heightAnchor.constraint(equalToConstant: 1),
             textFieldBorder.widthAnchor.constraint(equalTo: textField.widthAnchor),
             textFieldBorder.centerXAnchor.constraint(equalTo: textField.centerXAnchor),
-            textFieldBorder.topAnchor.constraint(equalTo: textField.bottomAnchor)
-        ])
-    }
-    
-    /// Sets up constraints for add button
-    private func setUpAddButtonConstraints() {
-        addButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            addButton.heightAnchor.constraint(equalToConstant: 40),
-            addButton.widthAnchor.constraint(equalToConstant: 60),
-            textFieldView.trailingAnchor.constraint(
-                equalToSystemSpacingAfter: addButton.trailingAnchor,
-                multiplier: 2),
-            addButton.leadingAnchor.constraint(
-                equalToSystemSpacingAfter: textField.trailingAnchor,
-                multiplier: 2),
+            textFieldBorder.topAnchor.constraint(equalTo: textField.bottomAnchor),
             addButton.bottomAnchor.constraint(
                 equalToSystemSpacingBelow: textFieldBorder.bottomAnchor,
                 multiplier: 0.1),
-            textFieldView.bottomAnchor.constraint(
-                equalToSystemSpacingBelow: addButton.bottomAnchor,
-                multiplier: 2)
         ])
     }
     
@@ -266,14 +318,14 @@ extension SearchFormViewController {
                 multiplier: 3),
             ingredientsView.leadingAnchor.constraint(equalTo: textFieldView.leadingAnchor),
             ingredientsView.trailingAnchor.constraint(equalTo: textFieldView.trailingAnchor),
-            view.safeAreaLayoutGuide.bottomAnchor.constraint(
+            container.bottomAnchor.constraint(
                 equalToSystemSpacingBelow: ingredientsView.bottomAnchor,
-                multiplier: 3)
+                multiplier: 3),
         ])
     }
     
     /// Sets up constraints for the ingredients title
-    private func setUpIngredientsTitleConstraints() {
+    private func setUpIngredientsTitleLabelConstraints() {
         ingredientsTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             ingredientsTitleLabel.topAnchor.constraint(
@@ -282,6 +334,10 @@ extension SearchFormViewController {
             ingredientsTitleLabel.leadingAnchor.constraint(
                 equalToSystemSpacingAfter: ingredientsView.leadingAnchor,
                 multiplier: 2),
+            ingredientsView.trailingAnchor.constraint(
+                equalToSystemSpacingAfter: ingredientsTitleLabel.trailingAnchor,
+                multiplier: 2),
+            
         ])
     }
     
@@ -302,11 +358,13 @@ extension SearchFormViewController {
     /// Sets up constraints for the clear button
     private func setUpClearButtonConstraints() {
         clearButton.translatesAutoresizingMaskIntoConstraints = false
+        let clearButtonTopAnchor = clearButton.topAnchor.constraint(
+                        equalToSystemSpacingBelow: ingredientsList.bottomAnchor,
+                        multiplier: 2)
+        clearButtonTopAnchor.priority = UILayoutPriority(rawValue: 1)
+        
         NSLayoutConstraint.activate([
-            clearButton.heightAnchor.constraint(equalToConstant: 50),
-            clearButton.topAnchor.constraint(
-                equalToSystemSpacingBelow: ingredientsList.bottomAnchor,
-                multiplier: 2),
+            clearButtonTopAnchor,
             clearButton.leadingAnchor.constraint(
                 equalToSystemSpacingAfter: ingredientsView.leadingAnchor,
                 multiplier: 2),
